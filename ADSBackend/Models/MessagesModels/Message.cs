86b04a7a68729d69f9c4f4bdc565c7c095d6f17d
Michael Tanczos <mike@logic-gate.com>
2019-09-05 14:54:45 -0400

    Added Message Model

---
+++ ADSBackend/Models/MessagesModels/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models.MessagesModels
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public string Author { get; set; }
+
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value.Replace("<br>", "\n");
+            }
+        }
+    }
+}
