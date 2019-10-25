9f57e60f795fef5a81f435665ed11f829e3a6ec6
Michael Tanczos <mike@logic-gate.com>
2019-10-25 14:15:26 -0400

    Added Game Points and moved result field

---
+++ ADSBackend/Models/MatchSetupViewModels/ManageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models.MatchSetupViewModels
+{
+    public class ManageViewModel
+    {
+        public Match Match { get; set; }
+    }
+}
