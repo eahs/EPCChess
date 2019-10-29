c75af302f24e6f9b6f20ca36467c4a951d25434a
Michael Tanczos <mike@logic-gate.com>
2019-10-29 15:15:19 -0400

    Adding Conference controller

---
+++ ADSBackend/Models/ConferenceViewModels/MatchResultsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models.ConferenceViewModels
+{
+    public class MatchResultsViewModel
+    {
+        public List<Division> Divisions { get; set; };
+        public List<Match> Matches { get; set; }
+    }
+}
