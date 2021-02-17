fdd7f0405df46f3f072e133bb8d46584ff613ce3
Michael Tanczos <mike@logic-gate.com>
2021-02-16 19:28:51 -0500

    Resolved game monitoring glitch and double click issue

--- ADSBackend/Services/GameMonitor.cs
+++ ADSBackend/Services/GameMonitor.cs
@@ -84,7 +84,7 @@ namespace ADSBackend.Services
 
                         }
 
-                        await ProcessGames(scope, vms, games);
+                        await ProcessGames(vms, games);
 
                         // Alert all hubs of game results
                         foreach (int matchId in vms.Keys)
@@ -115,193 +115,198 @@ namespace ADSBackend.Services
             }
         }
 
-        private async Task ProcessGames(IServiceScope scope, Dictionary<int, MatchUpdateViewModel> vms, Dictionary<string, Game> games)
+        private async Task ProcessGames(Dictionary<int, MatchUpdateViewModel> vms, Dictionary<string, Game> games)
         {
-            var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
-            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            var dataService = scope.ServiceProvider.GetRequiredService<DataService>();
-
-            // Get the API access token
-            var lichessAuthNSection = config.GetSection("Authentication:Lichess");
-            var appToken = lichessAuthNSection["AppToken"];
-
-            // Create a client that can use the token
-            var client = new LichessApiClient(appToken);
-
-            if (games.Keys.Count > 0)
+            using (IServiceScope scope = _provider.CreateScope())
             {
-                CancellationTokenSource cts = new CancellationTokenSource();
 
-                ExportGamesByIdsRequest request = new ExportGamesByIdsRequest
-                {
-                    PgnInJson = true,
-                    Moves = true,
-                    Clocks = true,
-                    Evals = false,
-                    Opening = true
-                };
+                var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var dataService = scope.ServiceProvider.GetRequiredService<DataService>();
 
-                try
+                // Get the API access token
+                var lichessAuthNSection = config.GetSection("Authentication:Lichess");
+                var appToken = lichessAuthNSection["AppToken"];
+
+                // Create a client that can use the token
+                var client = new LichessApiClient(appToken);
+
+                if (games.Keys.Count > 0)
                 {
-                    await foreach (LichessApi.Web.Models.Game ligame in client.Games
-                        .ExportGamesByIds(request, games.Keys.ToList(), cts.Token))
+                    CancellationTokenSource cts = new CancellationTokenSource();
+
+                    ExportGamesByIdsRequest request = new ExportGamesByIdsRequest
                     {
-                        // Remove this from the list
-                        games.Remove(ligame.Id);
+                        PgnInJson = true,
+                        Moves = true,
+                        Clocks = true,
+                        Evals = false,
+                        Opening = true
+                    };
 
-                        Chess chess = new Chess();
-
-                        if (!String.IsNullOrEmpty(ligame.Moves))
+                    try
+                    {
+                        await foreach (LichessApi.Web.Models.Game ligame in client.Games
+                            .ExportGamesByIds(request, games.Keys.ToList(), cts.Token))
                         {
-                            try
+                            Chess chess = new Chess();
+
+                            if (!String.IsNullOrEmpty(ligame.Moves))
                             {
-                                chess.loadSAN(ligame.Moves);
-                            }
-                            catch (Exception e)
-                            {
-                                Log.Error(e, "Error loading SAN {0}", ligame.Moves);
-                            }
-                        }
-
-                        if (games.ContainsKey(ligame.Id))
-                        {
-                            var game = games[ligame.Id];
-
-                            string fen = chess.Fen();
-
-                            game.CurrentFen = fen;
-                            game.LastMove = ligame.LastMoveAt;
-                            game.ChallengeStatus = ligame.Status.ToEnumString();
-                            game.ChallengeMoves = ligame.Moves ?? "";
-
-                            if (ligame.Status == GameStatus.Timeout ||
-                                ligame.Status == GameStatus.Aborted)
-                            {
-                                ResetGame(game);
-                            }
-                            else if (ligame.Status == GameStatus.Started)
-                            {
-                                game.IsStarted = true;
-                            }
-                            else if (ligame.Status == GameStatus.Draw ||
-                                     ligame.Status == GameStatus.Stalemate)
-                            {
-                                // Result: 0 = Draw, 1 = Home Win, 2 = Away Win, 3 = Reset
-                                GameResult result = GameResult.Draw;
-
-                                await dataService.UpdateAndLogRatingCalculations(game, result);
-
-                                game.Completed = true;
-                                game.CompletedDate = DateTime.Now;
-                            }
-                            else if (ligame.Status == GameStatus.OutOfTime ||
-                                     ligame.Status == GameStatus.Resign ||
-                                     ligame.Status == GameStatus.Mate)
-                            {
-                                GameResult result = GameResult.Draw;
-
-                                if (game.BoardPosition % 2 == 1)
+                                try
                                 {
-                                    // black player is home, white player is away
-                                    result = ligame.Winner.Equals("black")
-                                        ? GameResult.Player1Wins
-                                        : GameResult.Player2Wins;
+                                    chess.loadSAN(ligame.Moves);
                                 }
-                                else
+                                catch (Exception e)
                                 {
-                                    // white player is home, black player is away 
-                                    result = ligame.Winner.Equals("white")
-                                        ? GameResult.Player1Wins
-                                        : GameResult.Player2Wins;
+                                    Log.Error(e, "Error loading SAN {0}", ligame.Moves);
                                 }
+                            }
 
-                                // Everything beyond board 7 has flipped colors
-                                if (game.BoardPosition > 7)
+                            if (games.ContainsKey(ligame.Id))
+                            {
+                                var game = games[ligame.Id];
+
+                                string fen = chess.Fen();
+
+                                game.CurrentFen = fen;
+                                game.LastMove = ligame.LastMoveAt;
+                                game.ChallengeStatus = ligame.Status.ToEnumString();
+                                game.ChallengeMoves = ligame.Moves ?? "";
+
+                                if (ligame.Status == GameStatus.Timeout ||
+                                    ligame.Status == GameStatus.Aborted)
                                 {
-                                    if (result == GameResult.Player1Wins)
+                                    ResetGame(game);
+                                }
+                                else if (ligame.Status == GameStatus.Started)
+                                {
+                                    game.IsStarted = true;
+                                }
+                                else if (ligame.Status == GameStatus.Draw ||
+                                         ligame.Status == GameStatus.Stalemate)
+                                {
+                                    // Result: 0 = Draw, 1 = Home Win, 2 = Away Win, 3 = Reset
+                                    GameResult result = GameResult.Draw;
+
+                                    await dataService.UpdateAndLogRatingCalculations(game, result);
+
+                                    game.Completed = true;
+                                    game.CompletedDate = DateTime.Now;
+                                }
+                                else if (ligame.Status == GameStatus.OutOfTime ||
+                                         ligame.Status == GameStatus.Resign ||
+                                         ligame.Status == GameStatus.Mate)
+                                {
+                                    GameResult result = GameResult.Draw;
+
+                                    if (game.BoardPosition % 2 == 1)
                                     {
-                                        result = GameResult.Player2Wins;
+                                        // black player is home, white player is away
+                                        result = ligame.Winner.Equals("black")
+                                            ? GameResult.Player1Wins
+                                            : GameResult.Player2Wins;
                                     }
                                     else
                                     {
-                                        result = GameResult.Player1Wins;
+                                        // white player is home, black player is away 
+                                        result = ligame.Winner.Equals("white")
+                                            ? GameResult.Player1Wins
+                                            : GameResult.Player2Wins;
                                     }
 
+                                    // Everything beyond board 7 has flipped colors
+                                    if (game.BoardPosition > 7)
+                                    {
+                                        if (result == GameResult.Player1Wins)
+                                        {
+                                            result = GameResult.Player2Wins;
+                                        }
+                                        else
+                                        {
+                                            result = GameResult.Player1Wins;
+                                        }
+
+                                    }
+
+                                    await dataService.UpdateAndLogRatingCalculations(game, result);
+
+                                    game.Completed = true;
+                                    game.CompletedDate = DateTime.Now;
+                                }
+                                else if (ligame.Status == GameStatus.Cheat)
+                                {
+                                    game.CheatingDetected = true;
                                 }
 
-                                await dataService.UpdateAndLogRatingCalculations(game, result);
-
-                                game.Completed = true;
-                                game.CompletedDate = DateTime.Now;
-                            }
-                            else if (ligame.Status == GameStatus.Cheat)
-                            {
-                                game.CheatingDetected = true;
-                            }
-
-                            context.Game.Update(game);
-                            await context.SaveChangesAsync();
-
-                            // Add game to json output
-                            vms[game.MatchId].Games.Add(MapGameToJson(game));
-                        }
-
-                    }
-
-                }
-                catch (ApiInvalidRequestException e)
-                {
-                    Log.Error(e, "GameMonitor : Invalid Api request");
-                }
-                catch (ApiUnauthorizedException e)
-                {
-                    Log.Error(e, "GameMonitor : Unauthorized request");
-                }
-                catch (ApiRateLimitExceededException e)
-                {
-                    Log.Error(e, "GameMonitor: All requests are rate limited using various strategies, to ensure the API remains responsive for everyone. Only make one request at a time. If you receive an HTTP response with a 429 status, please wait a full minute before resuming API usage.");
-                    await Task.Delay(60 * 1000);  // Wait 60 seconds due to rate limit exceeded
-                }
-                catch (Exception e)
-                {
-                    Log.Error(e, "GameMonitor : Error occurred");
-                }
-
-                // Are there any games that we didn't get responses back for?
-                if (games.Keys.Count > 0)
-                {
-                    foreach (string gameId in games.Keys)
-                    {
-                        var game = games[gameId];
-
-                        if (game != null)
-                        {
-                            // This is to check for challenge games that are created but we would normally have to monitor
-                            // by streaming events for every user
-
-                            // Game is created but we aren't getting status updates on it
-                            var response = await client.Connector.SendRawRequest(new Uri(game.ChallengeUrl), HttpMethod.Get);
-                            var body = response.Body.ToString();
-
-                            if (body.Contains("Challenge canceled") ||
-                                body.Contains("Challenge declined"))
-                            {
-                                ResetGame(game);
-
                                 context.Game.Update(game);
-
                                 await context.SaveChangesAsync();
 
                                 // Add game to json output
                                 vms[game.MatchId].Games.Add(MapGameToJson(game));
+
+                                // Remove this from the list
+                                games.Remove(ligame.Id);
                             }
 
                         }
+
                     }
+                    catch (ApiInvalidRequestException e)
+                    {
+                        Log.Error(e, "GameMonitor : Invalid Api request");
+                    }
+                    catch (ApiUnauthorizedException e)
+                    {
+                        Log.Error(e, "GameMonitor : Unauthorized request");
+                    }
+                    catch (ApiRateLimitExceededException e)
+                    {
+                        Log.Error(e,
+                            "GameMonitor: All requests are rate limited using various strategies, to ensure the API remains responsive for everyone. Only make one request at a time. If you receive an HTTP response with a 429 status, please wait a full minute before resuming API usage.");
+                        await Task.Delay(60 * 1000); // Wait 60 seconds due to rate limit exceeded
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e, "GameMonitor : Error occurred");
+                    }
+
+                    // Are there any games that we didn't get responses back for?
+                    if (games.Keys.Count > 0)
+                    {
+                        foreach (string gameId in games.Keys)
+                        {
+                            var game = games[gameId];
+
+                            if (game != null)
+                            {
+                                // This is to check for challenge games that are created but we would normally have to monitor
+                                // by streaming events for every user
+
+                                // Game is created but we aren't getting status updates on it
+                                var response =
+                                    await client.Connector.SendRawRequest(new Uri(game.ChallengeUrl), HttpMethod.Get);
+                                var body = response.Body.ToString();
+
+                                if (body.Contains("Challenge canceled") ||
+                                    body.Contains("Challenge declined"))
+                                {
+                                    ResetGame(game);
+
+                                    context.Game.Update(game);
+
+                                    await context.SaveChangesAsync();
+
+                                    // Add game to json output
+                                    vms[game.MatchId].Games.Add(MapGameToJson(game));
+                                }
+
+                            }
+                        }
+                    }
+
                 }
-
             }
-
         }
 
         private void ResetGame(Game game)
